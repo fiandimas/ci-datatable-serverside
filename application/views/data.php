95d9b58db53590a6fc2b55ec939540a993955daa
Alfian Dimas <kidaliez@gmail.com>
2018-12-31 10:44:23 +0700

    update'

--- application/views/data.php
+++ application/views/data.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('assets/datatables/dataTables.bootstrap.css')?>">
-    <title>Welcome to CodeIgniter</title>
+    <title>Devil Fruit</title>
   </head>
   <body>
     <h1 style="text-align:center;">Devil Fruit User</h1>
